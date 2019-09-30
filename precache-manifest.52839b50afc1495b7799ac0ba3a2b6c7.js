fcbcd68ae5223f7ed59b908ecf8c720c645a2cab
dan0505 <qiaoer0505@gmail.com>
2019-09-30 17:11:46 +1000

    Updates

---
+++ precache-manifest.52839b50afc1495b7799ac0ba3a2b6c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c2b6256fb1eca6027da7930146813dc",
+    "url": "/todo_react/index.html"
+  },
+  {
+    "revision": "7079ffe9b05c28b5b69b",
+    "url": "/todo_react/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f8f2053378ae6ed3b3dc",
+    "url": "/todo_react/static/js/2.382c07d2.chunk.js"
+  },
+  {
+    "revision": "7079ffe9b05c28b5b69b",
+    "url": "/todo_react/static/js/main.15b9e3d0.chunk.js"
+  },
+  {
+    "revision": "50cf6b6edeafd871af36",
+    "url": "/todo_react/static/js/runtime-main.90b12301.js"
+  }
+]);
